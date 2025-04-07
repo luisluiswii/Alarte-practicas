5fecc8562ace434d90af19fc159152e812d6ca99
Carlos <carlosfraidias10@gmail.com>
2025-04-07 11:28:19 +0200

    Co-authored-by: luisluiswii <luisluiswii@users.noreply.github.com>

---
+++ ArchivosJS/prueba.js
@@ -0,0 +1,7 @@
+//Menu desplegable
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
