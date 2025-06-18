f210b27a07049a3879753880b4e40e2ff6c78471
luisluiswii-Dev <luissanchezgonzale95@gmail.com>
2025-06-18 12:46:51 +0200

    18/06/2025

--- ArchivosJS/prueba.js
+++
@@ -1,7 +0,0 @@
-//Menu desplegable
-const menuToggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
-
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
